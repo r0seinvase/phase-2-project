e52caa891b21b21f2c657f3a08963fd5b788069a
Paola Moreno Reyes <nailamoreno99@gmail.com>
2022-03-30 16:07:58 -0500

    new images

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,6 +1,12 @@
 import About from "./About";
 function Home({ home }) {
+
+    const aboutCard = home.map((info) => <About key={info.id} info={info} />)
+
+
     return <div>
+
+        {aboutCard}
 
     </div>
 
