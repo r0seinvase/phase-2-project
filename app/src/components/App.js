462fd59e2b945a15fe52a498824b2da0460fb182
David Caldera <davidcaldera13@yahoo.com>
2022-03-30 09:50:54 -0500

    squashed bugs witht the cards

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import { useState, useEffect } from "react";
 import '../App.css';
 import Item from "./Item"
 import Navbar from "./Navbar";
@@ -9,11 +10,28 @@ import Storage from "./Storage";
 import Electronics from "./Electronics";
 
 function App() {
+  const [seating, setSeating] = useState([])
+  const [storage, setStorage] = useState([])
+  const [electronics, setElectronics] = useState([])
+
+  useEffect(() => {
+      fetch("http://localhost:3000/chairs")
+          .then(res => res.json())
+          .then(data => setSeating(data))
+
+      fetch("http://localhost:3000/Storage")
+          .then(res => res.json())
+          .then(data => setStorage(data))
+
+      fetch("http://localhost:3000/Electronics")
+          .then(res => res.json())
+          .then(data => setElectronics(data))
+  }, []);
   return <div className="App">
     <div>
       <Navbar />
     </div>
-    <Home />
+    {/* <Home /> */}
     <Switch>
       <Route exact path="/Item">
         <Item />
@@ -25,13 +43,13 @@ function App() {
         <About />
       </Route>
       <Route exact path="/Seating">
-        <Seating />
+        <Seating seating={seating} />
       </Route>
       <Route exact path="/Storage">
-        <Storage />
+        <Storage storage={storage}/>
       </Route>
       <Route exact path="/Electronics">
-        <Electronics />
+        <Electronics electronics={electronics}/>
       </Route>
     </Switch>
   </div>
